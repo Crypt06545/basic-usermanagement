9d2a9d39ad82a0d59cad2464613ac55109cd424d
Crypt06545 <mehedihasansagor2005@gmail.com>
2024-11-28 10:11:37 +0600

    connect to mongodb

--- server/index.js
+++ server/index.js
@@ -1,39 +1,47 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const dotenv = require("dotenv");
 const express = require("express");
+const cors = require("cors");
+
+dotenv.config(); // Load environment variables
+
 const app = express();
 const port = 3000;
-const cors = require("cors");
 
 // middlewares
 app.use(cors());
 app.use(express.json());
 
-const users = [
-  { id: 1, name: "mehadi", email: "meahdi@gmail.com" },
-  { id: 2, name: "mehadi2", email: "meahdi2@gmail.com" },
-  { id: 3, name: "mehadi3", email: "meahdi3@gmail.com" },
-];
-// get requests
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@bot.jxczvgl.mongodb.net/?retryWrites=true&w=majority&appName=bot`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-app.get("/users", (req, res) => {
-  res.send(users);
-});
+async function run() {
+  try {
+    await client.connect();
 
-// post requests
-app.post("/users", (req, res) => {
-  //   res.send(users);
-  console.log("server hitting ");
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      res.send("User received");
+    });
 
-  console.log(req.body);
-  const newUSer = req.body;
-  newUSer.id = users.length + 1;
-  users.push(newUSer);
-  res.send(newUSer);
-});
-
-// console.log(users);
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
